5deb231fcf96dfaa6d0b22a8f631c43d9eaef850
hifikaa <rafikahekaa@gmail.com>
2021-07-30 19:51:39 +0700

    recake

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBVw7R5EEcERQV3UL6cRTy9d8_Nj0Ptq4k",
+    authDomain: "recake-8ece0.firebaseapp.com",
+    projectId: "recake-8ece0",
+    storageBucket: "recake-8ece0.appspot.com",
+    messagingSenderId: "548145189730",
+    appId: "1:548145189730:web:0f89c5d7b70be0e33b923b"
+    }
 };
